3e8909c7f9406e7f67e7e4500193dabf8bf890be
zhuhuanzi <1759042367@qq.com>
2023-07-27 17:59:19 +0800

    添加rabbitmq模块

---
+++ src/modules/Yoyo.Pro.EventBus.RabbitMQ/RabbitMQServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Yoyo.Pro
+{
+    public static class RabbitMQServiceExtensions
+    {
+        /// <summary>
+        /// 添加RabbitMq服务注册
+        /// </summary>
+        /// <param name="services"></param>
+        public static IServiceCollection AddRabbitMQService(this IServiceCollection services)
+        {
+            services.AddSingleton<IRabbitConnectionStore, RabbitConnectionStore>();
+            services.AddSingleton<IRabbitPolicyStore, RabbitPolicyStore>();
+            services.AddSingleton<IRabbitSettingStore, RabbitSettingStore>();
+            // 注册序列化服务
+            services.AddSingleton<IMsgPackSerializer, MsgPackSerializer>();
+            // 注册发布订阅服务
+            services.AddTransient<IRabbitSubscriber, RabbitSubscriber>();
+            services.AddTransient<IRabbitPublisher, RabbitPublisher>();
+
+            return services;
+        }
+    }
+}
