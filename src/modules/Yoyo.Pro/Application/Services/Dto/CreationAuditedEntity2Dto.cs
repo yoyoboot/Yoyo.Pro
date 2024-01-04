2784ab16d91d0191907cdb7e8e906d6a9832a65e
stan e <yi.hang@live.com>
2024-01-04 21:38:43 +0800

    增加审计Dto

---
+++ src/modules/Yoyo.Pro/Application/Services/Dto/CreationAuditedEntity2Dto.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Abp.Application.Services.Dto;
+using Yoyo.Pro.Entities.Auditing;
+
+namespace Yoyo.Pro.Application.Services.Dto
+{
+    public class CreationAuditedEntity2Dto<TPrimaryKey> : CreationAuditedEntityDto<TPrimaryKey>, ICreationNameAudited
+    {
+        public virtual string CreatorUserName { get; set; }
+    }
+}
