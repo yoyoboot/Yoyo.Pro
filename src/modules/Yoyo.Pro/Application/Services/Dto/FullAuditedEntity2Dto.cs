2130b4876cd88b4a14193a9b7a077bd8bd3e59b4
玩双截棍的熊猫 <yi.hang@live.com>
2024-03-04 13:15:27 +0800

    修改错误的dto命名

--- src/modules/Yoyo.Pro/Application/Services/Dto/FullAuditedEntity2Dto.cs
+++ src/modules/Yoyo.Pro/Application/Services/Dto/FullAuditedEntity2Dto.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,7 +7,7 @@ using Yoyo.Pro.Entities.Auditing;
 
 namespace Yoyo.Pro.Application.Services.Dto
 {
-    public class FullAuditedEntity2<TPrimaryKey> : FullAuditedEntityDto<TPrimaryKey>,
+    public class FullAuditedEntity2Dto<TPrimaryKey> : FullAuditedEntityDto<TPrimaryKey>,
         IDeletionNameAudited,
         ICreationNameAudited,
         IModificationNameAudited
