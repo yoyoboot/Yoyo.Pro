a6ce6313dce9327490223c4ea610aca29c2c8849
stan e <yi.hang@live.com>
2024-03-29 20:46:56 +0800

    移除EFCore.BulkExtensions的代码

--- tests/EFCore.BulkExtensions.Tests/IncludeGraph/Model/Spare.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EFCore.BulkExtensions.Tests.IncludeGraph.Model;
-
-public class Spare
-{
-    public int Id { get; set; }
-
-    public string PartNumber { get; set; } = null!;
-    public string Barcode { get; set; } = null!;
-}
